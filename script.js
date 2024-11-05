d6d821e59d0757614c04f202427dba8a41ad5ca2
stantonik <stanleyy.arnaud@gmail.com>
2024-11-05 17:05:30 +0100

    add zoom and css

--- script.js
+++ script.js
@@ -6,18 +6,19 @@
  */
 
 const gl_canvas = document.getElementById("context");
-const width = 1920.0;
-const height = 1080.0;
+const WIDTH = 1920.0;
+const HEIGHT = 1080.0;
+const ZOOM_SENSITIVITY = 0.01;
 var gl;
 var program;
+var uniform_loc = {}
 
 async function initgl()
 {
-        gl_canvas.height = height;
-        gl_canvas.width = width;
+        gl_canvas.height = HEIGHT;
+        gl_canvas.width = WIDTH;
 
         gl = gl_canvas.getContext("webgl2");
-
         if (gl === null)
         {
                 console.log("unable to initialize WebGL2");
@@ -55,6 +56,7 @@ async function initgl()
                 return;
         }
 
+        // Create program
         program = gl.createProgram();
         gl.attachShader(program, vertex);
         gl.attachShader(program, fragment);
@@ -68,15 +70,7 @@ async function initgl()
         }
 
         gl.useProgram(program);
-
-        const res_loc = gl.getUniformLocation(program, "iResolution");
-        gl.uniform2f(res_loc, width, height);
-
-        const sun_rot_loc = gl.getUniformLocation(program, "sun_rot");
-        document.getElementById("sun-rot").addEventListener('input', function(event) 
-                {
-                        gl.uniform1f(sun_rot_loc, parseFloat(event.target.value) * Math.PI / 180.0);
-                });
+        set_uniform_locations();
 
         // Create screen for ray marching
         const screen_vertex = new Float32Array([
@@ -92,34 +86,32 @@ async function initgl()
         gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
         gl.bufferData(gl.ARRAY_BUFFER, screen_vertex, gl.STATIC_DRAW);
 
+        gl.uniform2f(uniform_loc["iResolution"], WIDTH, HEIGHT);
+
         const positionLocation = gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 2 * 4, 0);
         gl.enableVertexAttribArray(positionLocation);
 
-        // Transfert mars radius data
+        // Transfert mars radius texture
         const map_res = 4;
         const line = map_res * 180;
         const line_sample = map_res * 360;
         const radius_array_8_3 = new Uint8Array(3 * line * line_sample);
 
         console.log("reading mars elevation data...");
-        response = await fetch("./mola_radius.txt");
-        text = await response.text();
+        response = await fetch("./megr90n000cb.img");
+        const array_buffer = await response.arrayBuffer();
+        const data_view = new DataView(array_buffer);
 
-        const radiusArray = text.split('\n').map(line => parseInt(line.trim(), 10))
-        const radius_array_32 = new Uint32Array(radiusArray);
-
-        const map_res_loc = gl.getUniformLocation(program, "mars_map_res");
-        gl.uniform1f(map_res_loc , parseFloat(map_res));
-
-        for (let i = 0; i < line * line_sample; i++)
+        for (let i = 0; i < data_view.byteLength; i += 2) 
         {
-                let byte1 = (radius_array_32[i] >> 16) & 0xFF; 
-                let byte2 = (radius_array_32[i] >> 8) & 0xFF;
-                let byte3 = radius_array_32[i] & 0xFF;
+                let j = i / 2;
+                // Read each 16-bit integer in big-endian format then convert it in uint32
+                let radius = (data_view.getInt16(i, false) + 3396000.0) >>> 0;
 
-                radius_array_8_3[i * 3] = byte1;
-                radius_array_8_3[i * 3 + 1] = byte2;
-                radius_array_8_3[i * 3 + 2] = byte3;
+                // Split it in 3 uint8
+                radius_array_8_3[j * 3] = (radius >> 16) & 0xFF;
+                radius_array_8_3[j * 3 + 1] = (radius >> 8) & 0xFF;
+                radius_array_8_3[j * 3 + 2] = radius & 0xFF;
         }
 
         const radius_texture = gl.createTexture();
@@ -138,90 +130,174 @@ async function initgl()
         gl.activeTexture(gl.TEXTURE0 + texture_unit);
         gl.bindTexture(gl.TEXTURE_2D, radius_texture);
 
-        const radius_tex_loc = gl.getUniformLocation(program, "radius_data");
-        gl.uniform1i(radius_tex_loc, texture_unit); 
+        gl.uniform1i(uniform_loc["radius_data"], texture_unit); 
+        gl.uniform1f(uniform_loc["mars_map_res"], parseFloat(map_res));
 
         console.log("ready");
 }
 
-var xpos = 0;
-var ypos = 0;
-var last_xpos = 0;
-var last_ypos = 0;
-var mouse_state = 0;
-var cam_roty = 0.0;
-var cam_rotx = 0.0;
-const fps_span = document.getElementById("fps");
-const slider = document.getElementById('floatSlider');
-
-let start_time;
-let last_time = 0;
-let timer = 0;
-
-function loop(timestamp)
+function set_uniform_locations()
 {
-        if (start_time === undefined) 
-        {
-                start_time = timestamp;
-        }
-
-        const delta_time = timestamp - last_time;
-
-        gl.clearColor(0, 0, 0, 1);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        const time_loc = gl.getUniformLocation(program, "iTime");
-        gl.uniform1f(time_loc , delta_time / 1000.0);
-
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-        if (timer >= 1000)
-        {
-                timer = 0;
-                fps_span.innerHTML = String(parseInt(1000.0 / delta_time));
-        }
-
-        timer += delta_time;
-
-        last_time = timestamp;
-        requestAnimationFrame(loop);
+        uniform_loc["iResolution"] = gl.getUniformLocation(program, "iResolution");
+        uniform_loc["sun_rot"] = gl.getUniformLocation(program, "sun_rot");
+        uniform_loc["radius_data"] = gl.getUniformLocation(program, "radius_data");
+        uniform_loc["mars_map_res"] = gl.getUniformLocation(program, "mars_map_res");
+        uniform_loc["cam_rot"] = gl.getUniformLocation(program, "cam_rot");
+        uniform_loc["cam_zoom"] = gl.getUniformLocation(program, "cam_zoom");
 }
 
 async function start()
 {
         await initgl();
 
+        // Handle mouse move
+        var xpos = 0;
+        var ypos = 0;
+        var last_xpos = 0;
+        var last_ypos = 0;
+        var mouse_state = 0;
+        var cam_roty = 0.0;
+        var cam_rotx = 0.0;
+        function handle_rotate(dx, dy)
+        {
+                cam_roty += dx / WIDTH * 2.0 * Math.PI;
+                cam_rotx += dy / HEIGHT * 2.0 * Math.PI;
+
+                if (cam_rotx > Math.PI / 2.0 - 0.1) cam_rotx = Math.PI / 2.0 - 0.1;
+                else if (cam_rotx < -Math.PI / 2.0 + 0.1) cam_rotx = -Math.PI / 2.0 + 0.1;
+
+                gl.uniform2f(uniform_loc["cam_rot"], cam_rotx, cam_roty);
+        }
+
+        function update_cursor_pos(event)
+        {
+                var rect = gl_canvas.getBoundingClientRect();
+                xpos = Math.floor((event.clientX - rect.left) * (WIDTH / rect.width));
+                ypos = Math.floor((event.clientY - rect.top) * (HEIGHT / rect.height));
+        }
+
+        gl_canvas.addEventListener("mousedown", (event) => {
+                mouse_state = 1;
+                update_cursor_pos(event);
+                last_xpos = xpos;
+                last_ypos = ypos;
+        });
+        gl_canvas.addEventListener("mouseup", () => mouse_state = 0);
         gl_canvas.addEventListener("mousemove", function(event)
                 {
-                        var rect = event.target.getBoundingClientRect();
-                        let x = Math.floor((event.clientX - rect.left) * (width / rect.width));
-                        let y = Math.floor((event.clientY - rect.top) * (height / rect.height));
-                        xpos = x;
-                        ypos = y;
                         if (mouse_state === 1)
                         {
-                                cam_roty += (xpos - last_xpos) / width * 2.0 * Math.PI;
-                                cam_rotx += (ypos - last_ypos) / height * 2.0 * Math.PI;
-                                const cam_rot_loc = gl.getUniformLocation(program, "cam_rot");
-                                gl.uniform2f(cam_rot_loc, cam_rotx, cam_roty);
+                                event.preventDefault();
+
+                                update_cursor_pos(event);
+                                handle_rotate(xpos - last_xpos, ypos - last_ypos);
+
                                 last_xpos = xpos;
                                 last_ypos = ypos;
                         }
-
-                        last_xpos = xpos;
-                        last_ypos = ypos;
                 })
 
-        gl_canvas.addEventListener("mousedown", function()
+        // Handle zoom and touch events
+        let cam_zoom = 2.0;
+        gl.uniform1f(uniform_loc["cam_zoom"], cam_zoom);
+        let lastDist = null;
+
+        gl_canvas.addEventListener("touchstart", (event) => {
+                if (event.touches.length === 1) 
                 {
                         mouse_state = 1;
-                });
-
-        gl_canvas.addEventListener("mouseup", function()
+                        const touch = event.touches[0];
+                        update_cursor_pos(touch);
+                        last_xpos = xpos;
+                        last_ypos = ypos;
+                }
+        });
+        gl_canvas.addEventListener("touchend", (event) => {
+                if (event.touches.length <= 1) lastDist = null; 
+                mouse_state = 0;
+        } );
+        gl_canvas.addEventListener("touchmove", function handleTouchMove(event)
                 {
-                        mouse_state = 0;
+                        event.preventDefault();
+                        if (mouse_state === 1 && event.touches.length === 1) {
+                                const touch = event.touches[0];
+
+                                update_cursor_pos(touch);
+                                handle_rotate(xpos - last_xpos, ypos - last_ypos);
+
+                                last_xpos = xpos;
+                                last_ypos = ypos;
+                        }
+                        else if (event.touches.length === 2) 
+                        {
+                                const touch1 = event.touches[0];
+                                const touch2 = event.touches[1];
+
+                                const dx = touch2.clientX - touch1.clientX;
+                                const dy = touch2.clientY - touch1.clientY;
+                                const currentDist = Math.sqrt(dx * dx + dy * dy);
+
+                                if (lastDist) 
+                                {
+                                        const zoomChange = (currentDist - lastDist) * ZOOM_SENSITIVITY;
+
+                                        cam_zoom += zoomChange;
+                                        if (cam_zoom < 0.1) cam_zoom = 0.1;
+                                        else if (cam_zoom > 5.0) cam_zoom = 5.0;
+
+                                        gl.uniform1f(uniform_loc["cam_zoom"], cam_zoom);
+                                }
+                                lastDist = currentDist;
+                        }
                 });
 
+        gl_canvas.addEventListener("wheel", function handleWheel(event) 
+                {
+                        event.preventDefault();
+
+                        cam_zoom += event.deltaY * -ZOOM_SENSITIVITY;
+                        if (cam_zoom < 0.1) cam_zoom = 0.1;
+                        else if (cam_zoom > 5.0) cam_zoom = 5.0;
+
+                        gl.uniform1f(uniform_loc["cam_zoom"], cam_zoom);
+                });
+
+        // Handle sun rot
+        document.getElementById("sun-rot").addEventListener('input', function(event) 
+                {
+                        gl.uniform1f(uniform_loc["sun_rot"], parseFloat(event.target.value) * Math.PI / 180.0);
+                });
+
+        var start_time;
+        var last_time = 0;
+        var timer = 0;
+
+        const fps_span = document.getElementById("fps");
+
+        function loop(timestamp)
+        {
+                if (start_time === undefined) start_time = timestamp;
+                const delta_time = (timestamp - last_time) / 1000.0;
+
+                gl.clearColor(0, 0, 0, 1);
+                gl.clear(gl.COLOR_BUFFER_BIT);
+
+                const time_loc = gl.getUniformLocation(program, "iTime");
+                gl.uniform1f(time_loc , delta_time);
+
+                gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+                if (timer == 0 || timer >= 0.5)
+                {
+                        timer = 0;
+                        fps_span.innerHTML = String(parseInt(1.0 / delta_time));
+                }
+
+                timer += delta_time;
+
+                last_time = timestamp;
+                requestAnimationFrame(loop);
+        }
         requestAnimationFrame(loop);
 }
 
--- shader.frag
+++ shader.frag
@@ -10,41 +10,37 @@ uniform usampler2D radius_data;
 uniform float mars_map_res;
 uniform float sun_rot;
 uniform vec2 cam_rot;
+uniform float cam_zoom;
 
 out vec4 color;
 
 #define MAX_DIST 100.0
-#define MIN_DIST 0.001
+#define MIN_DIST 1.0e-6
 #define NUMBER_OF_STEPS 128
 #define PI 3.1415926535897932384626433832795
 
 // Camera settings
-const vec3 cam_origin = vec3(0.0, 0.0, -3.0);
-const vec3 cam_target = vec3(0.0);
+vec3 cam_origin = vec3(0.0, 0.0, -3.0);
+vec3 cam_target = vec3(0.0);
 float cam_fov = 40.0;
 
-const vec3 light_color = vec3(1);
-const vec3 light_source = vec3(5.0, 0.0, 5.0);
+// Sun settings
+vec3 light_color = vec3(1);
+vec3 light_source = vec3(5.0, 0.0, 5.0);
 const float sun_radius = 3.0;
 
-const float mars_radius = 1.0;
+// Mars settings
 const vec3 mars_color = vec3(0.68, 0.2, 0.0);
 
+
 // Mathematicals function
-vec3 rotx(vec3 p, float theta)
+vec3 rotate(vec3 v, float angle, vec3 axis) 
 {
-        float yc = cos(theta) * p.y - sin(theta) * p.z;
-        float zc = sin(theta) * p.y + cos(theta) * p.z;
+        axis = normalize(axis);
+        float cosA = cos(angle);
+        float sinA = sin(angle);
 
-        return vec3(p.x, yc, zc);
-}
-
-vec3 roty(vec3 p, float theta)
-{
-        float xc = cos(theta) * p.x + sin(theta) * p.z;
-        float zc = -sin(theta) * p.x + cos(theta) * p.z;
-
-        return vec3(xc, p.y, zc);
+        return v * cosA + cross(axis, v) * sinA + axis * dot(axis, v) * (1.0 - cosA);
 }
 
 float map(vec3);
@@ -58,10 +54,39 @@ vec3 get_normal(vec3 p)
         return normalize(g);
 }
 
+float rand(vec3 st) 
+{
+        return fract(sin(dot(st, vec3(12.9898, 78.233, 45.164))) * 43758.5453);
+}
+
 // SDFs
 float sd_sphere(vec3 p, float r)
 {
         return length(p) - r;
+}
+
+// cf. inigo quilez
+float sd_stars(vec3 p)
+{
+        if (p.y > 30.0 || p.y < -30.0) return MAX_DIST;
+        p.y = fract(p.y) - 0.5;
+
+        const int n = 70;
+        const float b = 6.283185 / float(n);
+        float angle = atan(p.z, p.x);
+        float sectorIndex = floor(angle / b);
+
+        float angle1 = b * sectorIndex;
+        float angle2 = b * (sectorIndex + 1.0);
+
+        vec2 rotatedP1 = mat2(cos(angle1), -sin(angle1), sin(angle1), cos(angle1)) * p.xz;
+        vec2 rotatedP2 = mat2(cos(angle2), -sin(angle2), sin(angle2), cos(angle2)) * p.xz;
+
+        vec3 dec = vec3(30.0, 0.0, 0.0);
+        vec3 p1 = vec3(rotatedP1.x, p.y, rotatedP1.y) - dec;
+        vec3 p2 = vec3(rotatedP2.x, p.y, rotatedP2.y) - dec;
+
+        return min(sd_sphere(p1, 0.05), sd_sphere(p2, 0.05));
 }
 
 float sd_mars(vec3 p)
@@ -74,35 +99,42 @@ float sd_mars(vec3 p)
 
         vec2 uv = vec2(line_sample / 360.0, line / 180.0) / mars_map_res;
 
-        uvec3 radius_rgb = texture(radius_data, uv).xyz;
-        uint uradius = (radius_rgb.x << 16) | (radius_rgb.y << 8) | radius_rgb.z;
-        float radius = float(uradius);
+        uvec3 radius_rgb = texture(radius_data, uv).rgb;
+        float radius = float((radius_rgb.r << 16) | (radius_rgb.g << 8) | radius_rgb.b) / 3396000.0;
 
-        radius /= 3396000.0;
+        // Exagerate a bit the radius
+        return length(p) - pow(radius, 2.0);
+}
 
-        return length(p) - radius;
+float get_glow(float dist, float radius, float intensity)
+{
+        return pow(radius / max(dist, 1e-6), intensity);	
 }
 
 // Map
 float map(vec3 p)
 {
-        vec3 mars_pos = roty(p, cam_rot.y);
-        vec3 sun_pos = roty(light_source, -cam_rot.y + sun_rot) - p;
+        vec3 mars_pos = p;
+        vec3 sun_pos = light_source - p;
 
         float mars = sd_mars(mars_pos / 1.0) * 1.0;
         float sun = sd_sphere(sun_pos, sun_radius);
+        float stars = sd_stars(p);
 
-        return min(mars, min(sun, MAX_DIST));
+        return min(mars, min(sun, stars));
 }
 
 // Core
-float ray_marching(vec3 ro, vec3 rd, float max_dist)
+float ray_marching(vec3 ro, vec3 rd, float max_dist, inout float glow)
 {
         float dist = 0.0;
         for (int i = 0; i < NUMBER_OF_STEPS; i++)
         {
                 vec3 p = ro + rd * dist;     
                 float d = map(p);
+
+                float dsun = sd_sphere(light_source - p, sun_radius);
+                glow += get_glow(dsun, 5e-3, 0.8);
 
                 if (d < MIN_DIST) break;
 
@@ -118,62 +150,67 @@ vec3 render(vec2 uv)
 {
         vec3 color = vec3(0.0);
 
-        float scale = tan(radians(cam_fov * 0.5));
+        float scale = tan(radians(cam_fov * (cam_zoom * 0.4 + 0.4) * 0.5));
+
+        cam_origin = rotate(cam_origin - cam_target, cam_rot.y, vec3(0, 1, 0)) + cam_target;
 
         vec3 forward = normalize(cam_target - cam_origin);
         vec3 right = normalize(cross(vec3(0, 1, 0), forward));
+
+        cam_origin = rotate(cam_origin - cam_target, cam_rot.x, right) + cam_target;
+
+        forward = normalize(cam_target - cam_origin);
+        right = normalize(cross(vec3(0, 1, 0), forward));
         vec3 up = cross(forward, right);
 
-        vec3 rd = normalize(forward + uv.x * scale * right + uv.y * scale * up);
+        vec3 cam_dir = normalize(forward + uv.x * scale * right + uv.y * scale * up);
 
-        float dist = ray_marching(cam_origin, rd, MAX_DIST);
+        float glow = 0.0;
+        float dist = ray_marching(cam_origin, cam_dir, MAX_DIST, glow);
 
         if (dist < MAX_DIST)
         {
-                vec3 p = cam_origin + rd * dist;
+                vec3 p = cam_origin + cam_dir * dist;
 
-                vec3 rLight_source = roty(light_source, -cam_rot.y + sun_rot) - p;
                 // Ambient lighting
                 vec3 ambient = vec3(1.0); 
 
                 // Diffuse lightning
                 vec3 normal = get_normal(p);
-                float diffuse_strength = max(0.0, dot(normalize(rLight_source), normal));
+                float diffuse_strength = max(0.0, dot(normalize(light_source), normal));
                 vec3 diffuse = light_color * diffuse_strength;
 
                 // Specular lighting
-                vec3 reflect_source = normalize(reflect(-rLight_source, normal));
+                vec3 reflect_source = normalize(reflect(-light_source, normal));
                 float specular_strength = pow(max(0.0, dot(normalize(cam_origin), reflect_source)), 15.0);
                 vec3 specular = specular_strength * light_color;
 
-                vec3 lighting = vec3(0.0, 0.0, 0.0);
+                vec3 lighting = vec3(0.0);
                 lighting = ambient * 0.1 + diffuse * 0.8 + specular * 0.3;
 
                 // Colorise objects
-                if (sd_sphere(roty(light_source, -cam_rot.y + sun_rot) - p, sun_radius) < 0.1)
+                if (sd_stars(p) < 0.1)
                 {
-                        color = vec3(1.0, 0.8, 0.3) * 1.1;
+                        float k = rand(floor(p) * 2.0);
+                        if (k < 0.95) k = 0.0;
+
+                        color = vec3(1) * k;
                 }
                 else
                 {
                         color = mars_color * lighting;
-
-                        // Shadows
-                        // vec3 ro = p + normal * 0.1;
-                        // rd = normalize(rLight_source);
-                        // float dist_to_light = length(rLight_source - p);
-                        // dist = ray_marching(ro, rd, dist_to_light);
-                        // if (dist < dist_to_light) 
-                        // {
-                        //         color = color * vec3(0.25);
-                        // }
                 }
         }
         else
         {
-                // Background color
-                color = vec3(0.0);
+                color = vec3(0);
         }
+
+        // Add sun glow with screen blending
+        color = 1.0 - (1.0 - color) * (1.0 - glow * vec3(1.0, 0.9, 0.6));
+
+        // Gamma
+        // color = pow(color, vec3(0.4545));
 
         return color;
 }
@@ -182,6 +219,8 @@ void main()
 {
         vec2 uv = 2.0 * gl_FragCoord.xy / iResolution.xy - 1.0;
         uv.x *= iResolution.x / iResolution.y;
+
+        light_source = rotate(light_source, sun_rot, vec3(0.0, 1.0, 0.0));
 
         color = vec4(render(uv), 1.0);
 }
